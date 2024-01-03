733fe6bf5868d49207fed128c5b1f2deef8974eb
Hassan Jbara <hasan-account2012-3@live.com>
2024-01-03 16:51:32 +0100

    improve dataset function, try new tokenizer class for llama

--- training.py
+++ training.py
@@ -35,8 +35,7 @@ tqdm.pandas()
 class ScriptArguments:
     ppo_config: PPOConfig = field(
         default_factory=lambda: PPOConfig(
-            model_name="susnato/phi-1_5_dev",
-            query_dataset="imdb",
+            model_name="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
             reward_model="text-classification:Hello-SimpleAI/chatgpt-detector-roberta",
             learning_rate=1.41e-5,
             log_with=None,
@@ -80,40 +79,71 @@ trl_model_class = AutoModelForCausalLMWithValueHead if not args.use_seq2seq else
 # Below is an example function to build the dataset. In our case, we use the IMDB dataset
 # from the `datasets` library. One should customize this function to train the model on
 # its own dataset.
-def build_dataset(config, query_dataset, input_min_text_length=2, input_max_text_length=8):
+from transformers import AutoTokenizer
+from datasets import load_dataset
+
+def build_dataset(tokenizer, dataset_name="LDJnr/Pure-Dove", max_length=300):
     """
     Build dataset for training. This builds the dataset from `load_dataset`, one should
     customize this function to train the model on its own dataset.
 
     Args:
-        query_dataset (`str`):
+        dataset_name (`str`):
             The name of the dataset to be loaded.
 
     Returns:
         dataloader (`torch.utils.data.DataLoader`):
             The dataloader for the dataset.
     """
-    tokenizer = AutoTokenizer.from_pretrained(config.model_name)
+    tokenizer = AutoTokenizer.from_pretrained(config.model_name, trust_remote_code=True, padding_side='left') # padding left for encoder only models
     tokenizer.pad_token = tokenizer.eos_token
-    # load imdb with datasets
-    ds = load_dataset(query_dataset, split="train")
-    ds = ds.rename_columns({"text": "review"})
-    ds = ds.filter(lambda x: len(x["review"]) > 200, batched=False)
 
-    input_size = LengthSampler(input_min_text_length, input_max_text_length)
+    ds = load_dataset(dataset_name, split="train")
+    querys = [ds_item.get('conversation')[0].get('input') for ds_item in ds]
+    ds = ds.add_column('query', querys)
 
-    def tokenize(sample):
-        sample["input_ids"] = tokenizer.encode(sample["review"])[: input_size()]
-        sample["query"] = tokenizer.decode(sample["input_ids"])
-        return sample
-
-    ds = ds.map(tokenize, batched=False)
+    def prepare_dataset(ds_item):
+        prompt = [
+            {
+                "role": "system",
+                "content": "You are an assistant who gives detailed and long answers",
+            },
+            {
+                "role": "user", 
+                "content": ds_item['query']
+            },
+        ]
+        ds_item['query'] = tokenizer.apply_chat_template(prompt, tokenize=False, add_generation_prompt=True)
+        ds_item["input_ids"] = tokenizer.apply_chat_template(prompt, add_generation_prompt=True, return_tensors='pt', padding='max_length', max_length=max_length) 
+        # ds_item["query_ids"] = tokenizer.encode(ds_item["query"], padding='max_length', max_length=max_length)
+        return ds_item
+    
+    ds = ds.map(prepare_dataset, batched=False)
+    ds = ds.filter(lambda x: len(x["input_ids"]) <= max_length, batched=False)
+    ds = ds.remove_columns(['source', 'conversation'])
     ds.set_format(type="torch")
+    
     return ds
+
+tokenizer = AutoTokenizer.from_pretrained(script_args.tokenizer_name)
+# GPT-2 tokenizer has a pad token, but it is not eos_token by default. We need to set it to eos_token.
+# only for this model.
+
+if "llama" in script_args.tokenizer_name:
+    tokenizer.add_special_tokens(
+        {
+            "eos_token": DEFAULT_EOS_TOKEN,
+            "bos_token": DEFAULT_BOS_TOKEN,
+            "unk_token": DEFAULT_UNK_TOKEN,
+            "pad_token": DEFAULT_PAD_TOKEN,
+        }
+    )
+else:
+    tokenizer.pad_token = tokenizer.eos_token
 
 
 # We retrieve the dataloader by calling the `build_dataset` function.
-dataset = build_dataset(args.ppo_config, args.ppo_config.query_dataset)
+dataset = build_dataset(args.ppo_config, max_length=100)
 
 
 def collator(data):
@@ -180,12 +210,12 @@ if classifier_pipe.model.config.pad_token_id is None:
 # are passed to the `generate` function of the PPOTrainer, which is a wrapper around
 # the `generate` function of the trained model.
 generation_kwargs = {
-    "min_length": -1,
+    "min_length": 128,
     "top_k": 0.0,
     "top_p": 1.0,
     "do_sample": True,
     "pad_token_id": tokenizer.eos_token_id,
-    "max_new_tokens": 256,
+    "max_new_tokens": 512,
 }
 
 print(f"max new tokens: {generation_kwargs.get('max_new_tokens')}")
